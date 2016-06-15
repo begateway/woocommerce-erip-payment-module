dfa0008854dfcbada77ed99e79e165430ecf0d1c
avsh <avterus@gmail.com>
2016-06-15 22:04:19 +0300

    initial import

---
+++ erip-payments-plugin.php
@@ -0,0 +1,510 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) { 
+    exit; // Exit if accessed directly
+}
+
+/*
+	Класс для обработки callback от ЕРИП
+*/
+class WC_mygateway extends WC_Payment_Gateway {
+	public function __construct() {
+    	add_action('woocommerce_api_'.strtolower(get_class($this)), array(&$this, 'handle_callback'));
+  	}
+
+  	public function handle_callback() {
+
+  		$postData =  (string)file_get_contents("php://input");
+	    $post_array = json_decode($postData, false);
+	    $order_id = $post_array->transaction->tracking_id;
+	    $status = $post_array->transaction->status;
+
+	    global $woocommerce;
+		//Оплата будет происходить в ручном режиме
+		$order = new WC_Order( $order_id );
+
+		if ($order->id != 0) {
+			// Mark as on-hold (we're awaiting the cheque)
+			if ($post_array->transaction->status == 'successful') {
+				$order->update_status('Processing', 'Оплата через ЕРИП произведена');
+				echo 'Данные успешно получены! Заказ поставлен на выполнение.';
+			} else {
+				echo 'Данные успешно получены! Статус заказа не изменён.';
+			}			
+		} else {
+			echo 'Ошибка! Заказ не найден в системе.';
+		}
+/*
+	    if ($this->is_authorized() && $order_info) {
+	      if(isset($status) && $status == 'successful'){
+	        $message = $this->language->get('text_transaction_id') . ' ' . $transaction_id;
+	        $message .= PHP_EOL;
+	        $message .= $this->language->get('text_processor_message') . ' ' . $transaction_message;
+	        $this->model_checkout_order->addOrderHistory((int)$order_id, $this->config->get('begatewayerip_completed_status_id'), $message, false);
+	      }
+	    }
+*/
+    	die();
+	}
+}
+
+class SPYR_ERIP_GATEWAY extends WC_Payment_Gateway {
+	static private $optionsPlugin = null;
+
+	// Setup our Gateway's id, description and other values
+	function __construct() {
+		
+		// The global ID for this Payment method
+		$this->id = "SPYR_ERIP_GATEWAY";
+
+		// This basically defines your settings which are then loaded with init_settings()
+		$this->init_form_fields();
+
+		// After init_settings() is called, you can get the settings and load them into variables, e.g:
+		// $this->title = $this->get_option( 'title' );
+		$this->init_settings();
+		
+		// Turn these settings into variables we can use
+		foreach ( $this->settings as $setting_key => $value ) {
+			$this->$setting_key = $value;
+			self::$optionsPlugin[$setting_key] = $value;
+		}
+
+		// The Title shown on the top of the Payment Gateways Page next to all the other Payment Gateways
+		$this->method_title = __( "plugin_method_title", 'woocommerce-erip-payments' );
+
+		// The description for this Payment Gateway, shown on the actual Payment options page on the backend
+		$this->method_description = __( "plugin_method_description", 'woocommerce-erip-payments' );
+
+		// The title to be used for the vertical tabs that can be ordered top to bottom
+		$this->title = $this->get_option( 'name_sposoba_oplati' );
+
+		// If you want to show an image next to the gateway's name on the frontend, enter a URL to an image.
+		$this->icon = null;
+
+		// Bool. Can be set to true if you want payment fields to show on the checkout 
+		// if doing a direct integration, which we are doing in this case
+		$this->has_fields = true;
+
+		// Supports the default description
+		$this->supports = array('');
+
+		$this->description = wpautop( self::$optionsPlugin['description_sposoba_oplati'] );
+
+		// Customer Emails
+    	add_action( 'woocommerce_email_before_order_table', array( $this, 'email_instructions' ), 10, 3 );
+		
+		// Save settings
+		if ( is_admin() ) {
+			// Versions over 2.0
+			// Save our administration options. Since we are not going to be doing anything special
+			// we have not defined 'process_admin_options' in this class so the method in the parent
+			// class will be used instead
+			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+		}
+	} // End __construct()
+
+	static function thankyou_order_received_text_generate($order) {
+		//Проверяем режим работы обработки заказов плагина
+		if (self::$optionsPlugin['type_sposoba_oplati'] == 'manual') {
+			//В случае ручной обработки заказов, выдаём сообщение
+			return wpautop( self::$optionsPlugin['description_confiration_manual_mode'] );
+		} else {
+			global $woocommerce;
+
+			$api = new API;
+			$api->setDomainAPI( self::$optionsPlugin['erip_API_domain'] );
+			$api->setIdShop( self::$optionsPlugin['erip_id_magazin'] );
+			$api->setApiKeyShop( self::$optionsPlugin['erip_API_key'] );
+			//Получаем информацию о проведенном платеже в системе ЕРИП
+			$dataPaymentsEripSystem = $api->getInfoPaymentsWithOrderID($order->post->ID);
+			
+			//Замена плейсхолдеров на данные из отвера платёжной системы
+			$instructionEripPays = isset($dataPaymentsEripSystem->transaction->erip->instruction[0]) ? 
+										$dataPaymentsEripSystem->transaction->erip->instruction[0] 
+										: 
+										$dataPaymentsEripSystem->transaction->erip->instruction;
+
+			$message = wpautop( self::$optionsPlugin['description_confiration_auto_mode'] );
+			$message = str_replace("{{instruction_value_from_response}}", $instructionEripPays, $message);
+			$message = str_replace("{{order_number}}", $dataPaymentsEripSystem->transaction->order_id, $message);
+			return $message;
+		}
+	}
+
+	// Build the administration fields for this specific Gateway
+	public function init_form_fields() {
+		$this->form_fields = array(
+			//Включение платежного шлюза
+			'enabled' => array(
+				'title'		=> __( 'enable_disable_payments_gateway', 'woocommerce-erip-payments' ),
+				'label'		=> __( 'enable_disable_payments_gateway_label', 'woocommerce-erip-payments' ),
+				'type'		=> 'checkbox',
+				'default'	=> 'no',
+			),
+			//Id магазина
+			'erip_id_magazin' => array(
+				'title'		=> __( 'erip_id_magazin_title', 'woocommerce-erip-payments' ),
+				'type'		=> 'text',
+				'desc_tip'	=> __( 'erip_id_magazin_desk', 'woocommerce-erip-payments' ),
+				'placeholder'	=> '000',
+			),
+			//Ключ магазина
+			'erip_API_key' => array(
+				'title'		=> __( 'erip_API_key_title', 'woocommerce-erip-payments' ),
+				'type'		=> 'text',
+				'desc_tip'	=> __( 'erip_API_key_desk', 'woocommerce-erip-payments' ),
+				'default'	=> '',
+			),
+			//Домен API формата api.example.com
+			'erip_API_domain' => array(
+				'title'		=> __( 'erip_API_domain_title', 'woocommerce-erip-payments' ),
+				'type'		=> 'text',
+				'desc_tip'	=> __( 'erip_API_domain_desk', 'woocommerce-erip-payments' ),
+				'default'	=> 'api.bepaid.by',
+			),
+			//Код услуги  ЕРИП
+			'erip_kod_uslugi' => array(
+				'title'		=> __( 'erip_kod_uslugi_title', 'woocommerce-erip-payments' ),
+				'type'		=> 'text',
+				'desc_tip'	=> __( 'erip_kod_uslugi_desk', 'woocommerce-erip-payments' ),
+				'default'	=> '',
+			),
+			//Имя поставщивка услуги в ЕРИП
+			'erip_name_provider_uslugi' => array(
+				'title'		=> __( 'erip_name_provider_uslugi_title', 'woocommerce-erip-payments' ),
+				'type'		=> 'text',
+				'desc_tip'	=> __( 'erip_name_provider_uslugi_desk', 'woocommerce-erip-payments' ),
+				'default'	=> '',
+			),
+			//Информация для плательщика для печати на чеке
+			'info_message_in_check' => array(
+				'title'		=> __( 'info_message_in_check_title', 'woocommerce-erip-payments' ),
+				'type'		=> 'text',
+				'desc_tip'	=> __( 'info_message_in_check_desk', 'woocommerce-erip-payments' ),
+				'default'	=> __( 'info_message_in_check_default', 'woocommerce-erip-payments' ),
+			),
+			//Имя способа оплаты для покупателя на странице выбора
+			'name_sposoba_oplati' => array(
+				'title'		=> __( 'name_sposoba_oplati_title', 'woocommerce-erip-payments' ),
+				'type'		=> 'text',
+				'desc_tip'	=> __( 'name_sposoba_oplati_desk', 'woocommerce-erip-payments' ),
+				'default'	=> __( 'name_sposoba_oplati_default', 'woocommerce-erip-payments' ),
+			),
+			//Описание способа оплаты для покупателя
+			'description_sposoba_oplati' => array(
+				'title'		=> __( 'description_sposoba_oplati_title', 'woocommerce-erip-payments' ),
+				'type'		=> 'textarea',
+				'desc_tip'	=> __( 'description_sposoba_oplati_desk', 'woocommerce-erip-payments' ),
+				'default'	=> __( 'description_sposoba_oplati_default', 'woocommerce-erip-payments' ),
+				'css'		=> 'max-width:80%;'
+			),
+			//Текст-подтверждение заказа для ручного режима
+			'description_confiration_manual_mode' => array(
+				'title'		=> __( 'description_confiration_manual_mode_title', 'woocommerce-erip-payments' ),
+				'type'		=> 'textarea',
+				'desc_tip'	=> __( 'description_confiration_manual_mode_desk', 'woocommerce-erip-payments' ),
+				'default'	=> __( 'description_confiration_manual_mode_default', 'woocommerce-erip-payments' ),
+				'css'		=> 'max-width:80%;'
+			),
+			//Текст-подтверждение заказа для автоматического режима
+			'description_confiration_auto_mode' => array(
+				'title'		=> __( 'description_confiration_auto_mode_title', 'woocommerce-erip-payments' ),
+				'type'		=> 'textarea',
+				'desc_tip'	=> __( 'description_confiration_auto_mode_desk', 'woocommerce-erip-payments' ),
+				'default'	=> __( 'description_confiration_auto_mode_default', 'woocommerce-erip-payments' ),
+				'css'		=> 'max-width:80%;'
+			),
+			//Текст E-mail сообщения с информацией как оплатить заказ в системе ЕРИП
+			'description_erip_order_pay' => array(
+				'title'		=> __( 'description_erip_order_pay_title', 'woocommerce-erip-payments' ),
+				'type'		=> 'textarea',
+				'desc_tip'	=> __( 'description_erip_order_pay_desk', 'woocommerce-erip-payments' ),
+				'default'	=> __( 'description_erip_order_pay_default', 'woocommerce-erip-payments' ),
+				'css'		=> 'max-width:80%;'
+			),
+			//Создание счета в ЕРИП
+			'type_sposoba_oplati' => array(
+				'title'		=> __( 'type_invoice_create_title', 'woocommerce-erip-payments' ),
+		    	'desc'    	=> __( 'type_invoice_create_desk', 'woocommerce-erip-payments' ),
+		    	'css'     	=> 'min-width:150px;',
+		    	'std'     	=> 'left',
+		    	'default' 	=> 'left',
+		    	'type'    	=> 'select',
+		    	'options' 	=> array(
+		      		'manual'	=> __( 'manual_type_create_invoice_title', 'woocommerce-erip-payments' ),
+		      		'auto'		=> __( 'auto_type_create_invoice_title', 'woocommerce-erip-payments' ),
+		    	),
+		    	'desc_tip' 	=>  true,
+		  	),
+		);		
+	}
+
+	/*
+		Создание Счета на оплату в системе ЕРИП
+	*/
+	public function create_invoice_with_erip(&$order_sybmol_link) {
+		$moneyTool = new Money($order_sybmol_link->get_total(), $order_sybmol_link->get_order_currency());
+
+		$arrayDataInvoice = [
+ 			"request" => [
+ 				//@NOTICE: getAmount vs getCents
+				"amount" => $moneyTool->getCents(),
+				"currency" => $moneyTool->getCurrency(),
+				"description" => "Оплата заказа #".$order_sybmol_link->get_order_number(),
+				"email" => $order_sybmol_link->billing_email,
+				"ip" => Tools::getIp(),
+				"order_id" => $order_sybmol_link->get_order_number(),
+				"notification_url" => home_url('/').'wc-api/'.strtolower('WC_mygateway'),
+				"customer" => [
+					"first_name" => $order_sybmol_link->shipping_first_name,
+					"last_name" => $order_sybmol_link->shipping_last_name,
+					"country" => $order_sybmol_link->shipping_country,
+					"city" => $order_sybmol_link->shipping_city,
+					"zip" => $order_sybmol_link->shipping_postcode,
+					"address" => $order_sybmol_link->shipping_address_1.' '.$order_sybmol_link->shipping_address_2,
+					"phone" => $order_sybmol_link->billing_phone
+	 			],
+	 			"payment_method" => [
+					"type" => "erip",
+					"account_number" => $this->get_option( 'erip_id_magazin' ),
+					"service_no" => $this->get_option( 'erip_kod_uslugi' ),
+					"service_info" => [
+						"Уважаемый клиент,",
+						"Вы оплачиваете заказ ".$order_sybmol_link->get_order_number()
+					],
+					"receipt" => [
+						$this->get_option( 'info_message_in_check' )
+					]
+				]
+			]
+ 		];
+
+		$url = 'https://'.$this->get_option( 'erip_API_domain' ).'/beyag/payments';
+		$headers = array(
+            "Content-Type: application/json",
+            "Content-Length: " . strlen(json_encode($arrayDataInvoice)) 
+        );
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_PORT, 443);
+	    curl_setopt($ch, CURLOPT_HEADER, 0);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	    curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+	    curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+	    curl_setopt($ch, CURLOPT_POST, 1);
+	    curl_setopt($ch, CURLOPT_USERPWD, $this->get_option( 'erip_id_magazin' ).':'.$this->get_option( 'erip_API_key' ));
+	    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayDataInvoice));
+		$response = json_decode(curl_exec($ch));
+
+		if (!$response) {
+			wc_add_notice( curl_error($ch), 'error' );
+		}
+		curl_close($ch);
+
+		//Отправляем инструкцию как оплатить через ЕРИП
+		$this->sendInfoPaymentsErip($order_sybmol_link, $response);
+		update_post_meta($order_sybmol_link->get_order_number(), 'UID требования', $response->transaction->uid);
+
+		return ($response);
+	}
+
+	/*
+		Отправка сообщения с инструкцией об оплате
+	*/
+	protected function sendInfoPaymentsErip($order, $dataPaymentsEripSystem)
+	{
+		global $woocommerce;
+		if (!$order) return FALSE;
+
+	    $to_email = $order->billing_email;
+	    $headers = 'From: '. $order->billing_email . "\r\n";
+	    // Create a mailer
+      	$mailer = $woocommerce->mailer();
+
+      	$message_body = wpautop( wptexturize( $this->get_option( 'description_erip_order_pay' ) ) );
+		//Замена плейсхолдеров на данные из отвера платёжной системы
+		$instructionEripPays = isset($dataPaymentsEripSystem->transaction->erip->instruction[0]) ? 
+									$dataPaymentsEripSystem->transaction->erip->instruction[0] 
+									: 
+									$dataPaymentsEripSystem->transaction->erip->instruction;
+
+		$message_body = str_replace("{{instruction_erip}}", $instructionEripPays, $message_body);
+		$message_body = str_replace("{{order_num}}", $dataPaymentsEripSystem->transaction->order_id, $message_body);
+		$message_body = str_replace("{{fio}}", $order->shipping_first_name." ".$order->shipping_last_name, $message_body);
+		$message_body = str_replace("{{name_shop}}", get_option( 'blogname' ), $message_body);
+		$message_body = str_replace("{{name_provider_service}}", $this->get_option('erip_name_provider_uslugi'), $message_body);
+
+      	//$message = $mailer->wrap_message($message_body);
+      	$message 		= $mailer->wrap_message(
+        // Message head and message body.
+        sprintf('Инструкция об оплате заказа № %s', $order->get_order_number() ), $message_body );
+      	// Client email, email subject and message.
+		$result = $mailer->send( $order->billing_email, sprintf( 'Инструкция об оплате заказа № %s', $order->get_order_number() ), $message );
+	}
+
+	// Submit payment and handle response
+	public function process_payment( $order_id ) {
+		global $woocommerce;
+
+		if ($this->get_option( 'type_sposoba_oplati' ) == 'manual') {
+			//Оплата будет происходить в ручном режиме
+			$order = new WC_Order( $order_id );
+
+			// Mark as on-hold (we're awaiting the cheque)
+			$order->update_status('On-Hold', __( 'manual_mode_order_processing_title', 'woocommerce-erip-payments' ));
+
+			// Remove cart
+			$woocommerce->cart->empty_cart();
+
+			// Return thankyou redirect
+			return array(
+				'result' => 'success',
+				'redirect' => $this->get_return_url( $order )
+			);
+		} else {
+			//Оплата будет происходить в автоматическом режиме
+
+			//Помечаем как оплаченый
+			$order = new WC_Order( $order_id );
+
+			// Mark as on-hold (we're awaiting the cheque)
+			//$order->update_status('Pending payment', __( 'Ожидается оплата заказа', 'woocommerce-erip-payments' ));
+
+			// Reduce stock levels
+			$order->reduce_order_stock();
+
+			//Создаем заказ в системе ЕРИП
+			$dataResponseErip = $this->create_invoice_with_erip($order);
+
+			// Remove cart
+			//$woocommerce->cart->empty_cart();
+
+			// Return thankyou redirect
+			return array(
+				'result' => 'success',
+				'redirect' => $this->get_return_url( $order )
+			);
+		}
+	}
+
+    /**
+     * Add content to the WC emails.
+     *
+     * @access public
+     * @param WC_Order $order
+     * @param bool $sent_to_admin
+     * @param bool $plain_text
+     */
+	public function email_instructions( $order, $sent_to_admin, $plain_text = false ) {
+		var_dump($order->payment_method);
+		die();
+		if ( $this->instructions && ! $sent_to_admin && 'cod' === $order->payment_method ) {
+			echo wpautop( wptexturize( $this->instructions ) ) . PHP_EOL;
+		}
+	}
+}
+
+class Tools {
+	// lowercase first letter of functions. It is more standard for PHP
+	static function getIP() {
+	    if (isset($_SERVER)) {
+	        if (isset($_SERVER["HTTP_X_FORWARDED_FOR"]))
+	            return $_SERVER["HTTP_X_FORWARDED_FOR"];
+
+	        if (isset($_SERVER["HTTP_CLIENT_IP"]))
+	            return $_SERVER["HTTP_CLIENT_IP"];
+
+	        return $_SERVER["REMOTE_ADDR"];
+	    }
+
+	    if (getenv('HTTP_X_FORWARDED_FOR'))
+	        return getenv('HTTP_X_FORWARDED_FOR');
+
+	    if (getenv('HTTP_CLIENT_IP'))
+	        return getenv('HTTP_CLIENT_IP');
+
+	    return getenv('REMOTE_ADDR');
+	}
+}
+
+class Money {
+  protected $_amount;
+  protected $_currency;
+  protected $_cents;
+  public function __construct($amount = 0, $currency = 'USD') {
+    $this->_currency = $currency;
+    $this->setAmount($amount);
+  }
+  public function getCents() {
+    $cents = ($this->_cents) ? $this->_cents : (int)($this->_amount * $this->_currency_multiplyer());
+    return $cents;
+  }
+  public function setCents($cents) {
+    $this->_cents = (int)$cents;
+    $this->_amount = NULL;
+  }
+  public function setAmount($amount){
+    $this->_amount = (float)$amount;
+    $this->_cents = NULL;
+  }
+  public function getAmount() {
+    $amount = ($this->_amount) ? $this->_amount : (float)($this->_cents / $this->_currency_multiplyer());
+    return $amount;
+  }
+  public function setCurrency($currency){
+    $this->_currency = $currency;
+  }
+  public function getCurrency() {
+    return $this->_currency;
+  }
+  private function _currency_multiplyer() {
+    //array currency code => mutiplyer
+    $exceptions = array(
+        'BIF' => 1,
+        'BYR' => 1,
+        'CLF' => 1,
+        'CLP' => 1,
+        'CVE' => 1,
+        'DJF' => 1,
+        'GNF' => 1,
+        'IDR' => 1,
+        'IQD' => 1,
+        'IRR' => 1,
+        'ISK' => 1,
+        'JPY' => 1,
+        'KMF' => 1,
+        'KPW' => 1,
+        'KRW' => 1,
+        'LAK' => 1,
+        'LBP' => 1,
+        'MMK' => 1,
+        'PYG' => 1,
+        'RWF' => 1,
+        'SLL' => 1,
+        'STD' => 1,
+        'UYI' => 1,
+        'VND' => 1,
+        'VUV' => 1,
+        'XAF' => 1,
+        'XOF' => 1,
+        'XPF' => 1,
+        'MOP' => 10,
+        'BHD' => 1000,
+        'JOD' => 1000,
+        'KWD' => 1000,
+        'LYD' => 1000,
+        'OMR' => 1000,
+        'TND' => 1000
+    );
+    $multiplyer = 100; //default value
+    foreach ($exceptions as $key => $value) {
+        if (($this->_currency == $key)) {
+            $multiplyer = $value;
+            break;
+        }
+    }
+    return $multiplyer;
+  }
+}
---
+++ woocommerce-erip-payments.php
@@ -0,0 +1,103 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) { 
+    exit; // Exit if accessed directly
+}
+
+/*
+Plugin Name: WooCommerce ERIP Gateway Payments
+Plugin URI: #
+Description: Плагин для добавления шлюза оплаты ЕРИП
+Version: 1
+Author: Markun Vladislav
+Author Email: ihntlhi@gmail.com
+Text Domain: woocommerce-erip-payments
+*/
+
+define('PLUGIN_NAME_DOMAIN', 'spyr_erip_gateway');
+
+// Include our Gateway Class and register Payment Gateway with WooCommerce
+add_action( 'plugins_loaded', 'spyr_erip_gateway_init', 0 );
+function spyr_erip_gateway_init() {
+	// If the parent WC_Payment_Gateway class doesn't exist
+	// it means WooCommerce is not installed on the site
+	// so do nothing
+	if ( ! class_exists( 'WC_Payment_Gateway' ) ) return;
+	
+	// If we made it this far, then include our Gateway Class
+	include_once( 'erip-payments-plugin.php' );
+
+	//Подключение модели для работы с API
+	include_once( 'modelAPIErip.php' );
+
+	// Now that we have successfully included our class,
+	// Lets add it too WooCommerce
+	add_filter( 'woocommerce_payment_gateways', 'spyr_add_erip_gateway_gateway' );
+	add_filter( 'woocommerce_order_actions', 'wdm_add_order_meta_box_actions' );
+		
+	register_post_status( 'wc-shipped', array(
+         'label' => "Сгенерировать платежное получение в системе ЕРИП",
+         'public' => true,
+         'exclude_from_search' => false,
+         'show_in_admin_all_list' => true,
+         'show_in_admin_status_list' => true,
+         'label_count' => _n_noop( 'Shipped <span class="count">(%s)</span>', 'Shipped <span class="count">(%s)</span>' )
+        ));
+
+	/* Add Order action to Order action meta box */
+	    
+	function wdm_add_order_meta_box_actions($actions)
+	{
+	   $actions['wdm_shipped'] = "Сгенерировать платежное получение в системе ЕРИП";
+	   return $actions; 
+	}
+
+	function spyr_add_erip_gateway_gateway( $methods ) {
+		$methods[] = 'SPYR_ERIP_GATEWAY';
+		return $methods;
+	}
+}
+
+// Add custom action links
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'spyr_erip_gateway_action_links' );
+function spyr_erip_gateway_action_links( $links ) {
+	$plugin_links = array(
+		'<a href="' . admin_url( 'admin.php?page=wc-settings&tab=checkout&section='.PLUGIN_NAME_DOMAIN ) . '">' . __( 'settings', 'woocommerce-erip-payments' ) . '</a>',
+	);
+
+	// Merge our new link with the default ones
+	return array_merge( $plugin_links, $links );	
+}
+
+//Инициализация плагина и загрузка языкового пакета для плагина
+add_action("init", "pluginname_init");
+function pluginname_init() {
+	$res = load_plugin_textdomain("woocommerce-erip-payments", false, basename( dirname( __FILE__ ) ) . '/languages/');
+}
+
+/**
+ * Custom text on the receipt page.
+ */
+function isa_order_received_text( $text, $order ) {
+    return SPYR_ERIP_GATEWAY::thankyou_order_received_text_generate($order);
+}
+add_filter('woocommerce_thankyou_order_received_text', 'isa_order_received_text', 10, 2 );
+
+/**
+ * Обработчки Call Back
+ */
+function woocommerce_mygateway_add_gateway( $methods ) {
+  $methods[] = 'WC_mygateway';
+  return $methods;
+}
+add_filter( 'woocommerce_payment_gateways', 'woocommerce_mygateway_add_gateway');
+
+
+//Add callback if Shipped action called
+add_filter( 'woocommerce_order_action_wdm_shipped', 'wdm_order_shipped_callback', 10, 1);
+function wdm_order_shipped_callback($order)
+{ 
+    $plugin = new SPYR_ERIP_GATEWAY;
+    $order->update_status('Pending payment', __( 'Ожидание оплаты', 'woocommerce-erip-payments' ));
+    return $plugin->create_invoice_with_erip($order);
+}
