d61e97a6bdb200639f89eb31ef06ae24506aa747
avsh <avsh@users.noreply.github.com>
2024-02-18 13:46:26 +0300

    add composer to install begateway api php lib

---
+++ wc-bepaid-erip-payments/includes/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'begateway/wc-bepaid-erip-payments',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2f7b415dcaeb4da8001e775a6b1995b9413b0a28',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'begateway/begateway-api-php' => array(
+            'pretty_version' => '5.1.0',
+            'version' => '5.1.0.0',
+            'reference' => 'a58aa709182710f8eab692333131995e76beb808',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../begateway/begateway-api-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'begateway/wc-bepaid-erip-payments' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2f7b415dcaeb4da8001e775a6b1995b9413b0a28',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
